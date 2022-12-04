fd40a3e17b9340b3c23f76808103585e9dbe6cbc
בן שמואל <benshmuel@h-MacBook-Pro-sl-bn.local>
2022-12-04 02:43:00 +0200

    while-working

---
+++ portfolio/src/components/About/index.js
@@ -0,0 +1,83 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import {Loader} from 'react-loaders'
+// import{ BarLoader }from 'react-spinners'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCss3, faGitAlt, faHtml5, faJs, faJsSquare, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons'
+
+const About = () => {
+    const[letterClass,setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+       setTimeout(() => {
+         return   setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+
+    return (
+<div className='container about-page'>
+    <div className='text-zone'>
+<h1 className='title'>
+    <AnimatedLetters  
+    letterClass={letterClass}
+    strArray={['A','b','o','u','t',' ','m','e']}
+    idx={15}
+    />
+</h1>
+<p>
+I’m very ambitious FullStack developer seeking for my next position in
+            the high-tech industury with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+</p>
+<p>
+I'm quietly confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          
+</p>
+<p>
+
+If I need to define myself in one sentence that would be a freindly human 
+, resposeable, know how to run mission from the start to mthe end ,
+            working great under pressure and very loyal and commmit to the place that I work in.
+</p>
+    </div>
+
+    <div className='stage-cube-cont'>
+       < div className='cubespinner'>
+        <div className='face1'>
+            <FontAwesomeIcon icon={faReact} color="#5Ed4f4"/>
+
+        </div>
+        <div className='face2'>
+            <FontAwesomeIcon icon={faNodeJs} color="#91c63d"/>
+
+        </div>
+    
+        <div className='face3'>
+            <FontAwesomeIcon icon={faJsSquare} color="#efd81d"/>
+
+        </div>
+        <div className='face4'>
+            <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
+
+        </div>
+        <div className='face5'>
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
+
+        </div>
+        <div className='face6'>
+            <FontAwesomeIcon icon={faGitAlt} color="#ec4d28"/>
+
+        </div>
+
+       </div>
+
+    </div>
+    {/* <BarLoader color="#36d7b7" /> */}
+    <Loader type="pacman"/>
+</div>
+    )
+}
+
+export default About
---
+++ portfolio/src/components/Home/index.js
@@ -0,0 +1,72 @@
+ import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/bletterpng.png'
+import AnimatedLetters from '../AnimatedLetters';
+import'./index.scss';
+import Logo from './Logo/index'
+
+const Home =()=>{
+    const[letterClass,setLetterClass] = useState('text-animate')
+    const nameArray = ['e','n']
+    const jobArray = [
+    'f',
+    'u',
+    'l',
+    'l',
+    's',
+    't',
+    'a',
+    'c',
+    'k',
+    ' ',
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.']
+
+useEffect(() => {
+    setTimeout(() => {
+        setLetterClass('text-animate-hover')
+    }, 3000)
+}, [])
+    
+ 
+ return(
+    <div className="container home-page">
+        <div className="text-zone">
+    <h1>  
+        <span className={letterClass}>H</span>
+        <span className={`${letterClass}_12`}>i</span>
+         <br/> 
+         <span className={`${letterClass}_13`}>I</span>
+        <span className={`${letterClass}_14`}>'m</span>
+    <img src={LogoTitle} alt="developer"/>
+    <AnimatedLetters letterClass={letterClass}
+   strArray={nameArray}
+   idx={15}/>
+    <br/>
+   <AnimatedLetters letterClass={letterClass}
+   strArray={jobArray}
+   idx={22}/>
+    </h1>
+    <h2>
+        FullStack Developer / MERN Stack : MongoDb , Express.Js , React , Node.Js</h2>
+        <Link to='/contact' className='flat-button'>CONACT ME </Link>
+</div>
+<Logo />
+    </div>
+ )
+ }
+
+
+ export default Home
